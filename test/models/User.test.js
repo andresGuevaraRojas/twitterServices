462e42700d91c6fc3ce0ee8c696282a5e5684811
Andres Guevara <=>
2022-04-23 19:54:13 -0500

    Se agrega el test para el modelo Twitter

---
+++ test/models/User.test.js
@@ -0,0 +1,32 @@
+const User = require('../../app/models/User')
+describe('Unit test for User class',()=>{
+    test('Create an User object',()=>{
+        const user = new User(1,'carlogilmar','Carlo','Bio')
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe('carlogilmar')
+        expect(user.name).toBe('Carlo')
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
+    })
+
+    test('Add getters',()=>{
+        const user = new User(1,'carlogilmar','Carlo','Bio')
+
+        expect(user.getUserName).toBe('carlogilmar')
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    })
+
+    test("Add setters",()=>{
+        const user = new User(1,'carlogilmar','Carlo','Bio')
+
+        user.setUserName = "Gilmar"
+        expect(user.username).toBe("Gilmar")
+
+        user.setBio = 'New bio'
+        expect(user.bio).toBe('New bio')
+    })
+})
