bb7e21d1ac8c5f23ad29cc6ca149953bba4880b4
Andres Guevara <=>
2022-04-23 22:24:33 -0500

    Se escribe el test de createUser de la vista UserView para el caso en que se envia un payload con propriedades invalidas

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -9,4 +9,11 @@ describe("Test for UserView",()=>{
         expect(result.error).toMatch(/payload no existe/)
 
     })    
+
+    test("Return an error object when try to create a new user with payload with invalid properties",()=>{
+        const payload = {username:null,name:null,id:'id'}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
+    
 })
